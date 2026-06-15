476f5e65ecf1ca06fe0c0311408387edca5540a5
navanitha-csbs <eswaranavanitha@gmail.com>
2026-06-15 16:19:04 +0530

    Added Program 26: Reverse Number using While Loop in C

---
+++ 04_Loops/26_Reverse_Number.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main()
+{
+    int num, digit, reverse = 0;
+
+    printf("Enter a number: ");
+    scanf("%d", &num);
+
+    while(num != 0)
+    {
+        digit = num % 10;
+
+        reverse = reverse * 10 + digit;
+
+        num = num / 10;
+    }
+
+    printf("Reversed Number = %d\n", reverse);
+
+    return 0;
+}
